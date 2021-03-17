634be5f2a3b40811303dac47a87c2b8ee977ff21
lajippx <80778566+lajippx@users.noreply.github.com>
2021-03-17 18:11:27 +0800

    Update jmx.sh

--- .profile.d/jmx.sh
+++ .profile.d/jmx.sh
@@ -4,8 +4,8 @@
 BIN_DIR=`pwd`
 cd ..
 DEPLOY_DIR=`pwd`
+pwd
 CLASSES=WEB-INF/classes
-echo "apm home: $APM_AGENT_HOME"
 MAIN=im.zhaojun.zfile.ZfileApplication
 JMX_HOST_NAME=0.0.0.0
 JMX_PORT=1099
@@ -15,27 +15,8 @@ JAVA_MEM_OPTS=" -Djava.security.egd=file:/dev/./urandom -Dfile.encoding=utf-8 "
 
 SERVER_NAME="zfile-3.0.0"
 
-if [ -z "$SERVER_NAME" ]; then
-    SERVER_NAME=`hostname`
-fi
 
-PIDS=`ps aux | grep java | grep "$CLASSES" |awk '{print $2}'`
-if [ -n "$PIDS" ]; then
-    echo "ERROR: The $SERVER_NAME already started!"
-    echo "PID: $PIDS"
-    exit 1
-fi
-
-if [ -n "$SERVER_PORT" ]; then
-    SERVER_PORT_COUNT=`netstat -tln | grep $SERVER_PORT | wc -l`
-    if [ $SERVER_PORT_COUNT -gt 0 ]; then
-        echo "ERROR: The $SERVER_NAME port $SERVER_PORT already used!"
-        exit 1
-    fi
-fi
-
-
-LIB_DIR=$DEPLOY_DIR/WEB-INF/lib
+LIB_DIR=./WEB-INF/lib
 LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`
 
 JAVA_OPTS=" -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT+08"
