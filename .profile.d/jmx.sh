bf5506d1b11d7414f02610379ecc59cdc38cc06d
lajippx <80778566+lajippx@users.noreply.github.com>
2021-03-17 18:00:15 +0800

    Update jmx.sh

--- .profile.d/jmx.sh
+++ .profile.d/jmx.sh
@@ -4,8 +4,6 @@ jmx_port=${JMX_PORT:-"1098"}
 rmi_port=${RMI_PORT:-"1099"}
 ip_addr="$(ip -4 a show eth0 | grep inet | sed -E -e 's/.*inet //g' | sed -E -e 's/\/[0-9]+.*//g')"
 
-[ -z "$SSH_CLIENT" ] && curl -sSL ${TUNNELS_URL} | bash
-
 export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS}\
    -Dcom.sun.management.jmxremote\
    -Dcom.sun.management.jmxremote.port=${jmx_port}\
